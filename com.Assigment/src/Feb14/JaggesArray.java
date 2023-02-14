071e6c68a96043df7c389918dd13f1c50cdba684
balbir <balbirjob@gmail.com>
2023-02-14 22:41:03 +0530

    Example of jagges array using 2D

--- com.Assigment/src/Feb14/JaggesArray.java
+++ com.Assigment/src/Feb14/JaggesArray.java
@@ -1,17 +1,21 @@
-package Feb13;
+package Feb14;
 import java.util.Scanner;
-public class TwoDArry2 {
+public class JaggesArray {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		String arr[][]=new String[7][5];
-		for(int i=0;i<=arr.length-1;i++)
+		int arr[][]=new int[4][];
+		arr[0]=new int[4];
+		arr[1]=new int[2];
+		arr[2]=new int[3];
+		arr[3]=new int[5];
+				for(int i=0;i<=arr.length-1;i++)
 		{
 			for(int j=0;j<=arr[i].length-1;j++) 
 			{
-		      System.out.println("Enter the bank:"+i +" and name of customer: "+j );
+		      System.out.println("Enter age of "+i +" Student : "+j );
 		    
-		         arr[i][j]=s.next();
+		         arr[i][j]=s.nextInt();
 			}
 		}
 		System.out.println("Array containt are ");
@@ -25,4 +29,5 @@ public class TwoDArry2 {
 		}
 		s.close();
 	}
-}   
+
+}
